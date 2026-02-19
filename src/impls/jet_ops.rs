83bbc00e62a2dee852c690e7901057c2453049d0
Joachim Moeyens <moeyensj@gmail.com>
2026-02-18 18:00:48 -0800

    Finish Jet<O, N> +,-,*,/ f64 implementations up to 2nd order

--- src/impls/jet_ops.rs
+++ src/impls/jet_ops.rs
@@ -168,7 +168,6 @@ impl<const O: usize, const N: usize> Sub<Jet<O, N>> for f64 {
                 result.hess[i] = -rhs.hess[i];
             }
         }
-
         result
     }
 }
@@ -194,7 +193,6 @@ impl<const O: usize, const N: usize> Mul<f64> for Jet<O, N> {
                 result.hess[i] = self.hess[i] * rhs;
             }
         }
-
         result
     }
 }
@@ -220,21 +218,29 @@ impl<const O: usize, const N: usize> Div<Jet<O, N>> for f64 {
     type Output = Jet<O, N>;
 
     fn div(self, rhs: Jet<O, N>) -> Jet<O, N> {
-        let a_inv = 1.0 / rhs.value;
-
         let mut result = Jet {
-            value: rhs.value * a_inv,
+            value: self / rhs.value,
             grad: [0.0; N],
             hess: [0.0; MAX_HESS_STORAGE],
         };
 
         if O >= 1 {
             for i in 0..N {
-                result.grad[i] = rhs.grad[i] * a_inv * a_inv * -self;
+                result.grad[i] = (-result.value * rhs.grad[i]) / rhs.value;
             }
         }
 
-        // TODO: Implement Hessian division
+        if O >= 2 {
+            for i in 0..N {
+                for j in 0..=i {
+                    let idx = hess_index(i, j).unwrap();
+                    result.hess[idx] = (-result.value * rhs.hess[idx]
+                        - result.grad[i] * rhs.grad[j]
+                        - result.grad[j] * rhs.grad[i])
+                        / rhs.value;
+                }
+            }
+        }
         result
     }
 }
